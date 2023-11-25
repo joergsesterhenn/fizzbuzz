2560f12998fcbea8da46f5aa177860cb714ec241
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-11-25 22:39:22 +0100

    new test fails: return buzz for 5

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -6,6 +6,9 @@ class FizzBuzzTestCase(unittest.TestCase):
         reply = main.fizzbuzz(3)
         self.assertEqual("Fizz", reply)
 
+    def test_returns_buzz_for_5(self):
+        reply = main.fizzbuzz(5)
+        self.assertEqual("Buzz", reply)
 
 if __name__ == '__main__':
     unittest.main()
