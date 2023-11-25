a09b753563f0fcd694166b87372c32ed68e490ba
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-11-26 00:01:12 +0100

    refactored to use dict - stage 1 finished: 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz

--- fizzbuzz/main.py
+++ fizzbuzz/main.py
@@ -1,14 +1,26 @@
-def fizzbuzz(number):
-    reply = ""
-    if number % 3 == 0:
-        reply += "Fizz"
+numbers_to_mark = {3: "Fizz", 5: "Buzz"}
 
-    if number % 5 == 0:
-        reply += "Buzz"
-    return reply if reply != "" else str(number)
+
+def fizzbuzz(number):
+    marker_for_divisibility = ""
+    for divisor in numbers_to_mark:
+        marker_for_divisibility += append_marker(number, divisor)
+    return marker_for_divisibility \
+        if marker_for_divisibility != "" \
+        else str(number)
+
+
+def append_marker(number, divisor):
+    if dividable_by(number, divisor):
+        return numbers_to_mark[divisor]
+    else:
+        return ""
+
+
+def dividable_by(number, divisor):
+    return number % divisor == 0
 
 
 if __name__ == '__main__':
     for input_number in range(100):
         print(fizzbuzz(input_number+1))
-
