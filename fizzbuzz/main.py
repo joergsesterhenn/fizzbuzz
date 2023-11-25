03af8d62a893717302a17b5909e21177b7efc59a
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-11-25 22:04:12 +0100

    initial commit

---
+++ fizzbuzz/main.py
@@ -0,0 +1,10 @@
+def fizzbuzz():
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi there')  # Press Strg+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    fizzbuzz()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
