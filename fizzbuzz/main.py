34a81f78ebdb87a4abb0cac55105b5514a4e1031
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-11-26 00:45:37 +0100

    test green

--- fizzbuzz/main.py
+++ fizzbuzz/main.py
@@ -1,23 +1,41 @@
+import string
+
 numbers_to_mark = {3: "Fizz", 5: "Buzz"}
 
 
-def fizzbuzz(number):
-    marker_for_divisibility = ""
+def fizzbuzz(number) -> string:
+    marker_for_dividability = ""
+    marker_for_being_part_of = ""
+    marker = ""
     for divisor in numbers_to_mark:
-        marker_for_divisibility += append_marker(number, divisor)
-    return marker_for_divisibility \
-        if marker_for_divisibility != "" \
-        else str(number)
+        marker_for_dividability += (
+            append_marker_for_dividability(number, divisor))
+    for part in numbers_to_mark:
+        marker_for_being_part_of += (
+            append_marker_for_being_part_of(number, part))
+        marker = marker_for_being_part_of + marker_for_dividability
+    return marker if marker != "" else str(number)
 
 
-def append_marker(number, divisor):
+def append_marker_for_dividability(number, divisor) -> string:
     if dividable_by(number, divisor):
         return numbers_to_mark[divisor]
     else:
         return ""
 
 
-def dividable_by(number, divisor):
+def append_marker_for_being_part_of(number, part) -> string:
+    if is_part_of(number, part):
+        return numbers_to_mark[part]
+    else:
+        return ""
+
+
+def is_part_of(number, character) -> bool:
+    return str(number).find(str(character)) != -1
+
+
+def dividable_by(number, divisor) -> bool:
     return number % divisor == 0
 
 
