e233762819762a19235f2360e5a3641069b7576f
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-11-25 23:08:07 +0100

    Failing test: 6 should return Fizz

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -3,18 +3,29 @@ from fizzbuzz import main
 
 
 class FizzBuzzTestCase(unittest.TestCase):
+    def test_returns_number_for_1(self):
+        reply = main.fizzbuzz(1)
+        self.assertEqual("1", reply)
+
+    def test_returns_number_for_2(self):
+        reply = main.fizzbuzz(2)
+        self.assertEqual("2", reply)
+
     def test_returns_fizz_for_3(self):
         reply = main.fizzbuzz(3)
         self.assertEqual("Fizz", reply)
+
+    def test_returns_number_for_4(self):
+        reply = main.fizzbuzz(4)
+        self.assertEqual("4", reply)
 
     def test_returns_buzz_for_5(self):
         reply = main.fizzbuzz(5)
         self.assertEqual("Buzz", reply)
 
-    def test_returns_number_for_1(self):
-        reply = main.fizzbuzz(1)
-        self.assertEqual("1", reply)
-
+    def test_returns_fizz_for_6(self):
+        reply = main.fizzbuzz(6)
+        self.assertEqual("Fizz", reply)
 
 if __name__ == '__main__':
     unittest.main()
