4a7da57552e3225ce229a188505c7d478b098be7
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-12-06 14:29:12 +0100

    add test

--- tests/test_number_marker.py
+++ tests/test_number_marker.py
@@ -1,9 +1,15 @@
 import unittest
 
-from fizzbuzz.number_marker import DivisibleNumberMarker, ContainsNumberMarker
+from fizzbuzz.number_marker import (DivisibleNumberMarker,
+                                    ContainsNumberMarker,
+                                    CrossSumDivisibleNumberMarker)
 
 divisible_number_marker = DivisibleNumberMarker(
     {3: "Fizz", 5: "Buzz", 7: "Whizz", 11: "Bang"}
+)
+
+cross_sum_divisible_number_marker = CrossSumDivisibleNumberMarker(
+    {3: "Fizz", 5: "Buzz"}
 )
 
 contains_number_marker = ContainsNumberMarker(
@@ -55,6 +61,60 @@ class DivisibleNumberMarkerTestCase(unittest.TestCase):
     def test_returns_fizzbuzz_for_15(self):
         reply = divisible_number_marker.mark(15)
         self.assertEqual("FizzBuzz", reply)
+
+
+class CrossSumDivisibleNumberMarkerTestCase(unittest.TestCase):
+    def test_returns_number_for_1(self):
+        reply = cross_sum_divisible_number_marker.mark(1)
+        self.assertEqual("1", reply)
+
+    def test_returns_number_for_2(self):
+        reply = cross_sum_divisible_number_marker.mark(2)
+        self.assertEqual("2", reply)
+
+    def test_returns_fizz_for_3(self):
+        reply = cross_sum_divisible_number_marker.mark(3)
+        self.assertEqual("Fizz", reply)
+
+    def test_returns_number_for_4(self):
+        reply = cross_sum_divisible_number_marker.mark(4)
+        self.assertEqual("4", reply)
+
+    def test_returns_buzz_for_5(self):
+        reply = cross_sum_divisible_number_marker.mark(5)
+        self.assertEqual("Buzz", reply)
+
+    def test_returns_fizz_for_6(self):
+        reply = cross_sum_divisible_number_marker.mark(6)
+        self.assertEqual("Fizz", reply)
+
+    def test_returns_numebr_for_7(self):
+        reply = cross_sum_divisible_number_marker.mark(7)
+        self.assertEqual("7", reply)
+
+    def test_returns_number_for_8(self):
+        reply = cross_sum_divisible_number_marker.mark(8)
+        self.assertEqual("8", reply)
+
+    def test_returns_fizz_for_9(self):
+        reply = cross_sum_divisible_number_marker.mark(9)
+        self.assertEqual("Fizz", reply)
+
+    def test_returns_number_for_10(self):
+        reply = cross_sum_divisible_number_marker.mark(10)
+        self.assertEqual("10", reply)
+
+    def test_returns_fizz_for_12(self):
+        reply = cross_sum_divisible_number_marker.mark(12)
+        self.assertEqual("Fizz", reply)
+
+    def test_returns_fizz_for_15(self):
+        reply = cross_sum_divisible_number_marker.mark(15)
+        self.assertEqual("Fizz", reply)
+
+    def test_returns_buzz_for_15(self):
+        reply = cross_sum_divisible_number_marker.mark(23)
+        self.assertEqual("Buzz", reply)
 
 
 class ContainsNumberMarkerTestCase(unittest.TestCase):
