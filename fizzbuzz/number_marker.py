37475c68fef2fc3af82a7819f48a394b29f7a32b
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-12-06 11:56:40 +0100

    add docs

--- fizzbuzz/number_marker.py
+++ fizzbuzz/number_marker.py
@@ -3,6 +3,13 @@ from abc import ABCMeta, abstractmethod
 
 
 class NumberMarker(metaclass=ABCMeta):
+    """Baseclass for marking numbers with strings.
+
+       Attributes:
+           numbers_to_mark: A dictionary that maps the numbers that are to be
+                            marked to the strings that are to be used as mark.
+       """
+
     numbers_to_mark = {}
 
     def __init__(self, numbers_to_mark):
@@ -20,6 +27,13 @@ class NumberMarker(metaclass=ABCMeta):
 
 
 class DivisibleNumberMarker(NumberMarker):
+    """Marks divisible numbers with strings.
+
+       Attributes:
+           numbers_to_mark: A dictionary that maps the numbers that are to be
+                            marked to the strings that are to be used as mark.
+       """
+
     def lookup_marker(self, number: int, number_to_mark: int) -> string:
         if self.__divisible_by(number, number_to_mark):
             return self.numbers_to_mark[number_to_mark]
@@ -32,6 +46,13 @@ class DivisibleNumberMarker(NumberMarker):
 
 
 class ContainsNumberMarker(NumberMarker):
+    """Marks numbers that contain specific digits with strings.
+
+       Attributes:
+           numbers_to_mark: A dictionary that maps the numbers that are to be
+                            marked to the strings that are to be used as mark.
+       """
+
     def lookup_marker(self, number: int, number_to_mark: int) -> string:
         if self.__contains(number, number_to_mark):
             return self.numbers_to_mark[number_to_mark]
