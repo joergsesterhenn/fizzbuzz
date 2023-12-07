d83938fe18ceb53cc2aebf71ed28863267b78fcb
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-12-07 09:22:47 +0100

    refactor main

--- fizzbuzz/main.py
+++ fizzbuzz/main.py
@@ -1,4 +1,6 @@
-from fizzbuzz.number_marker import DivisibleNumberMarker, ContainsNumberMarker
+from fizzbuzz.number_marker import (DivisibleNumberMarker,
+                                    ContainsNumberMarker,
+                                    CrossSumDivisibleNumberMarker)
 
 if __name__ == '__main__':
     value = ""
@@ -8,16 +10,24 @@ if __name__ == '__main__':
     contains_number_marker = ContainsNumberMarker(
         {3: "Fizz", 5: "Buzz"}
     )
+    cross_divisible_number_marker = CrossSumDivisibleNumberMarker(
+        {3: "Fizz", 5: "Buzz"}
+    )
+
     input_number = 1
     while value != "FizzBuzzWhizzBangFizzBuzz":
         div_mark = divisible_number_marker.mark(input_number)
         cont_mark = contains_number_marker.mark(input_number)
-        if div_mark == str(input_number):
-            value = cont_mark
-        elif cont_mark == str(input_number):
-            value = div_mark
-        else:
-            value = div_mark + cont_mark
+        cross_mark = cross_divisible_number_marker.mark(input_number)
+
+        value = ""
+        for mark in {div_mark, cont_mark, cross_mark}:
+            if mark == str(input_number):
+                pass
+            else:
+                value += mark
+        if value == "":
+            value = str(input_number)
+
         print(value)
-        # print(input_number)
         input_number += 1
